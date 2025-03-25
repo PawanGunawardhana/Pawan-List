ba9a9f4145b8c243a8849fe959402d37bbbc463a
Pawan Gunawardhana <adpigunawardhana2@gmail.com>
2025-03-25 14:13:03 +0530

    Added components for reuse

---
+++ components/Footer.js
@@ -0,0 +1,5 @@
+const Footer = () => {
+  return <footer className="footer">Copyright &copy; 2025 People List</footer>;
+};
+
+export default Footer;
---
+++ pages/about.js
@@ -0,0 +1,11 @@
+const About = () => {
+  return (
+    <div>
+          <h1>About</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime consequatur officiis officia quis! Velit, delectus saepe deserunt quasi ipsam architecto.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime consequatur officiis officia quis! Velit, delectus saepe deserunt quasi ipsam architecto.</p>
+    </div>
+  );
+};
+
+export default About;
