43af84e800003ab4b3b4fb9d134150f5ec33af9a
Pawan Gunawardhana <adpigunawardhana2@gmail.com>
2025-03-26 19:26:51 +0530

    Define Dynamic Routes using getStaticPaths and fetched single items

--- pages/people/[id].js
+++ pages/people/[id].js
@@ -1,9 +1,42 @@
-const Details = () => {
-    return (
-        <div>
-            <h1>Details Page</h1>
-        </div>
-    );
-}
- 
+export const getStaticPaths = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
+  const paths = data.map((person) => {
+    return {
+      params: { id: person.id.toString() },
+    };
+  });
+  return {
+    paths: paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  const res = await fetch("https://jsonplaceholder.typicode.com/users/" + id);
+  const data = await res.json();
+
+  return {
+    props: { person: data },
+  };
+};
+
+const Details = ({ person }) => {
+  return (
+      <div>
+          <h1>Person Details</h1>
+          <div>
+              <h3>{ person.name }</h3>
+              <p>{person.email}</p>
+              <p>{person.phone}</p>
+              <p>{person.address.city}</p>
+              <p>{person.website}</p>
+
+          </div>
+    </div>
+  );
+};
+
 export default Details;
