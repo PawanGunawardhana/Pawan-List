3ba1197ac04f38640e54cdfffd80b5c4eb88dbc0
Pawan Gunawardhana <adpigunawardhana2@gmail.com>
2025-03-26 17:58:23 +0530

    Fetched data using getStaticProps

--- pages/people/index.js
+++ pages/people/index.js
@@ -1,7 +1,28 @@
-const People = () => {
+import styles from "../../styles/Peoples.module.css";
+
+export const getStaticProps = async () => {
+  // this function runs at build time as our app is built and our component rendered
+  const res = await fetch("https://jsonplaceholder.typicode.com/users"); //async method
+  const data = await res.json(); //this is also async method, this is an array of objects
+
+  return {
+    props: { people: data },
+  };
+};
+
+const People = ({ people }) => {
   return (
     <div>
       <h1>People</h1>
+      {people.map((person) => {
+        return (
+          <div key={person.id}>
+            <a className={styles.single}>
+              <h3>{person.name}</h3>
+            </a>
+          </div>
+        );
+      })}
     </div>
   );
 };
